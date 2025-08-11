9793f5e9308e5c7a34a63a4459a3aaf4e68e0340
Alisson986826 <alissonnants@gmail.com>
2025-08-11 19:23:35 +0000

    desafio batalha naval nivel  aventureiro finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,39 +7,63 @@ int main() {
         for(j = 0; j < 10; j++){
             tabuleiro[i][j] = 0;
         }
+    
     }
+
+    int tamanho_navio = 3;
+
     //coordenadas dos navios
-    int linha_navio_horizontal = 2;
-    int coluna_navio_horizontal = 4;
-    int linha_navio_vertical = 5;
-    int coluna_navio_vertical = 7;
-    //posicões dos navios
-    for (j = 0; j < 3; j++) {
-        tabuleiro[linha_navio_horizontal][coluna_navio_horizontal + j] = 3;
-    }
+    int l1 = 2, c1 = 4;
+    int l2 = 5, c2 = 7;
+    int l3 = 0, c3 = 0;
+    int l4 = 0, c4 = 9;
+    #define LIVRE(l,c) (tabuleiro[l][c] == 0)
 
-    for (i = 0; i < 3; i++){
-        if (tabuleiro[linha_navio_vertical + i][coluna_navio_vertical] == 0){
-            tabuleiro[linha_navio_vertical + i][coluna_navio_vertical] = 3;
+    if (c1 + tamanho_navio <= 10) {
+        for (j = 0; j < tamanho_navio; j++) {
+            if (LIVRE(l1, c1 + j))
+            tabuleiro[l1][c1 + j] = 3;
         }
     }
-    //Exibindo o tabuleiroo
+     if (l2 + tamanho_navio <= 10) {
+        for (i = 0; i < tamanho_navio; i++) {
+            if (LIVRE(l2 + i, c2))
+            tabuleiro[l2 + i][c2] = 3;
+        } 
+    }
+     if (l3 + tamanho_navio <= 10 && c3 + tamanho_navio <= 10) {
+        for (i = 0; i < tamanho_navio; i++) {
+            if (LIVRE(l3 + i, c3 + i))
+            tabuleiro[l3 + i][c3 + i] = 3; 
+        } 
+    }
+     if (l4 + tamanho_navio <= 10 && c4 - (tamanho_navio - 1) >= 0) {
+        for (i = 0; i < tamanho_navio; i++) {
+            if (LIVRE(l4 + i, c4 - i))
+            tabuleiro[l4 + i][c4 - i] = 3; 
+        } 
+    }
+
+    //Exibindo tabuleiro
     printf("    ");
-    for (j = 0; j < 10; j++){
-        printf("%c ", 'A' + j);}
-
-        printf("\n");
-
-        for (i = 0; i < 10; i++) {
-            printf("%2d  ",i);
-            for (j = 0; j < 10; j++) {
-                printf("%d ", tabuleiro[i][j]);
-            }
-            printf("\n");
-        }
-
-        return 0;
+    for (j = 0; j < 10; j++) {
+        printf("%c ", 'A' + j);
     }
+    printf("\n");
+
+    for(i = 0; i < 10; i++) {
+        printf("%2d  ", i);
+        for (j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0; 
+}
+
+
+
 
 
 
