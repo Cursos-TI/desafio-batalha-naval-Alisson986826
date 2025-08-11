96dc3beec36ceae91ea32df0669d4185f1396ecf
Alisson986826 <alissonnants@gmail.com>
2025-08-11 20:06:16 +0000

    desafio batalha naval nivel mestre finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,69 +1,105 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h> // para abs()
+
+#define TAM 10   // tamanho do tabuleiro
+#define HAB 5    // tamanho das matrizes de habilidade
+
+void aplicaHabilidade(int tabuleiro[TAM][TAM], int matriz[HAB][HAB], int origem_l, int origem_c) {
+    int i, j;
+    for (i = 0; i < HAB; i++) {
+        for (j = 0; j < HAB; j++) {
+            if (matriz[i][j] == 1) {
+                int tl = origem_l + i - HAB / 2;
+                int tc = origem_c + j - HAB / 2;
+                if (tl >= 0 && tl < TAM && tc >= 0 && tc < TAM && tabuleiro[tl][tc] == 0)
+                    tabuleiro[tl][tc] = 5; // área de efeito
+            }
+        }
+    }
+}
 
 int main() {
-    int tabuleiro[10][10];
+    int tabuleiro[TAM][TAM];
+    int cone[HAB][HAB];
+    int cruz[HAB][HAB];
+    int octaedro[HAB][HAB];
     int i, j;
-    for (i = 0; i < 10; i++) {
-        for(j = 0; j < 10; j++){
+
+    // Inicializa o tabuleiro com água (0)
+    for (i = 0; i < TAM; i++) {
+        for (j = 0; j < TAM; j++) {
             tabuleiro[i][j] = 0;
         }
-    
     }
 
-    int tamanho_navio = 3;
+    // Coloca alguns navios (valor 3)
+    tabuleiro[2][4] = 3;
+    tabuleiro[2][5] = 3;
+    tabuleiro[2][6] = 3;
 
-    //coordenadas dos navios
-    int l1 = 2, c1 = 4;
-    int l2 = 5, c2 = 7;
-    int l3 = 0, c3 = 0;
-    int l4 = 0, c4 = 9;
-    #define LIVRE(l,c) (tabuleiro[l][c] == 0)
+    tabuleiro[5][7] = 3;
+    tabuleiro[6][7] = 3;
+    tabuleiro[7][7] = 3;
 
-    if (c1 + tamanho_navio <= 10) {
-        for (j = 0; j < tamanho_navio; j++) {
-            if (LIVRE(l1, c1 + j))
-            tabuleiro[l1][c1 + j] = 3;
+    // Gera cone (origem no topo central)
+    for (i = 0; i < HAB; i++) {
+        for (j = 0; j < HAB; j++) {
+            if (j >= (HAB / 2) - i && j <= (HAB / 2) + i)
+                cone[i][j] = 1;
+            else
+                cone[i][j] = 0;
         }
     }
-     if (l2 + tamanho_navio <= 10) {
-        for (i = 0; i < tamanho_navio; i++) {
-            if (LIVRE(l2 + i, c2))
-            tabuleiro[l2 + i][c2] = 3;
-        } 
-    }
-     if (l3 + tamanho_navio <= 10 && c3 + tamanho_navio <= 10) {
-        for (i = 0; i < tamanho_navio; i++) {
-            if (LIVRE(l3 + i, c3 + i))
-            tabuleiro[l3 + i][c3 + i] = 3; 
-        } 
-    }
-     if (l4 + tamanho_navio <= 10 && c4 - (tamanho_navio - 1) >= 0) {
-        for (i = 0; i < tamanho_navio; i++) {
-            if (LIVRE(l4 + i, c4 - i))
-            tabuleiro[l4 + i][c4 - i] = 3; 
-        } 
+
+    // Gera cruz (origem no centro)
+    for (i = 0; i < HAB; i++) {
+        for (j = 0; j < HAB; j++) {
+            if (i == HAB / 2 || j == HAB / 2)
+                cruz[i][j] = 1;
+            else
+                cruz[i][j] = 0;
+        }
     }
 
-    //Exibindo tabuleiro
+    // Gera octaedro (losango)
+    for (i = 0; i < HAB; i++) {
+        for (j = 0; j < HAB; j++) {
+            if (abs(i - HAB / 2) + abs(j - HAB / 2) <= HAB / 2)
+                octaedro[i][j] = 1;
+            else
+                octaedro[i][j] = 0;
+        }
+    }
+
+    // Posições de origem das habilidades no tabuleiro
+    int origem_cone_l = 0, origem_cone_c = 0;
+    int origem_cruz_l = 4, origem_cruz_c = 4;
+    int origem_octa_l = 7, origem_octa_c = 2;
+
+    // Aplica cada habilidade no tabuleiro
+    aplicaHabilidade(tabuleiro, cone, origem_cone_l, origem_cone_c);
+    aplicaHabilidade(tabuleiro, cruz, origem_cruz_l, origem_cruz_c);
+    aplicaHabilidade(tabuleiro, octaedro, origem_octa_l, origem_octa_c);
+
+    // Exibe cabeçalho com letras
     printf("    ");
-    for (j = 0; j < 10; j++) {
+    for (j = 0; j < TAM; j++) {
         printf("%c ", 'A' + j);
     }
     printf("\n");
 
-    for(i = 0; i < 10; i++) {
+    // Exibe tabuleiro
+    for (i = 0; i < TAM; i++) {
         printf("%2d  ", i);
-        for (j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]);
+        for (j = 0; j < TAM; j++) {
+            if (tabuleiro[i][j] == 0) printf(". "); // água
+            else if (tabuleiro[i][j] == 3) printf("N "); // navio
+            else if (tabuleiro[i][j] == 5) printf("* "); // habilidade
         }
         printf("\n");
     }
 
-    return 0; 
+    return 0;
 }
-
-
-
-
 
 
